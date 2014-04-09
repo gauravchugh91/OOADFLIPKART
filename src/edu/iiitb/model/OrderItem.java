ecadf94ab05b5e42c3684fdefe8866f7595cf04a
prathyusha83 <nlpsid@gmail.com>
2014-04-09 15:16:54 +0530

    pushed

---
+++ src/edu/iiitb/model/OrderItem.java
@@ -0,0 +1,18 @@
+package edu.iiitb.model;
+
+import java.util.Date;
+
+public class OrderItem {
+	
+ int orderitemid;
+ int orderid;
+ int productid;
+ int quantity;
+ int price;
+ String orderstatus;
+ Date deliverydate;
+ Date returndate;
+ int subtotal;
+ 
+ 
+}
